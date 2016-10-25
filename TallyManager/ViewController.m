68c39bb9577b333857f5a32c7695876c546a259c
DonlerSun <13238041502@163.com>
2016-10-25 20:31:15 +0800

    Initial Commit

---
+++ TallyManager/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  TallyManager
+//
+//  Created by Apple on 16/10/25.
+//  Copyright © 2016年 Apple. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
